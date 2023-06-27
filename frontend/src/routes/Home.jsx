89fd5b967b6cfdbf956b1de24842a2781fbd465f
Ale <alexmanson12345@gmail.com>
2023-06-27 13:13:34 -0300

    fix:Removing bugs from the Home route

--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -18,43 +18,54 @@ function Home() {
 
       setPosts(data);
     } catch (err) {
-      const errorMessage = document.createElement('p');
-      errorMessage.textContent = `Ocorreu um erro: ${err}`;
-      document.body.appendChild(errorMessage);
+      console.log(err);
     }
   };
 
   useEffect(() => {
     if (!localStorage.getItem('login')) navigate('/login');
+
     getPosts();
-  }, []);
+  }, [reload]);
 
   const deletePost = async (post) => {
     await blogFetch.delete(`/posts/${post}`);
     setReload(!reload);
   };
 
+  function getDate(date) {
+    const [data, hora] = date.split('T');
+    const hour = hora.substr(0, 8);
+    const newVariavel = `${data} ${hour}`;
+    const newDate = new Date(newVariavel);
+    const longMonth = newDate.toLocaleString('pt-br', { month: 'long' });
+    return `${newDate.getDate()} de ${longMonth} ás ${hour}`;
+  }
+
   return (
     <div className="home">
-      <h1>Últimos Posts</h1>
+      <h1>Últimos posts</h1>
       {posts.length === 0 ? (
-        <p>Nenhum post encontrado</p>
-      ) : (posts.map((post) => (
-        <div key={post.id} className="post">
-          <h2>{post.title}</h2>
-          <p className="span-container">
-            <span>{`${post.owner}`}</span>
-            <span>{`${post.created_at}`}</span>
-          </p>
-          <p className="quebra-linha">
-            {post.body.length > 120 ? `${post.body.substr(0, 120)}...` : post.body}
-          </p>
-          <div className="container-btn">
-            <GoTo post={post} deletePost={deletePost} />
-            <DeleteBtn post={post} deletePost={deletePost} />
+        <p>Que triste, nada postado ainda!</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p className="span-container">
+              <span>{`${post.owner} `}</span>
+              <span>{getDate(post.created_at)}</span>
+            </p>
+            <p className="quebra-linha">
+              {post.body.length > 120
+                ? `${post.body.substr(0, 120)} ...` : post.body}
+            </p>
+            <div className="container-btn">
+              <GoTo post={post} deletePost={deletePost} />
+              <DeleteBtn post={post} deletePost={deletePost} />
+            </div>
           </div>
-        </div>
-      )))}
+        ))
+      )}
     </div>
   );
 }
