444f93cf52a8dfffd5a496cf8de34b9db135d0c3
Ale <alexmanson12345@gmail.com>
2023-06-27 13:35:11 -0300

    fix: Removing bugs from the ReadMore route

--- frontend/src/routes/ReadMore.jsx
+++ frontend/src/routes/ReadMore.jsx
@@ -1,23 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import blogFetch from '../axios/config';
 import DeleteBtn from '../components/DeleteBtn';
 import GoTo from '../components/GoTo';
-import blogFetch from '../axios/config';
+import '../css/routes/ReadMore.css';
 
 function ReadMore() {
   const navigate = useNavigate();
   const { id } = useParams();
   const [post, setPost] = useState();
-  const [reload, setReload] = useState();
+  const [reload, setReload] = useState(false);
 
   async function getPost(ids) {
     const { data } = await blogFetch.get(`/posts/${ids}`);
+
     setPost(data);
   }
-
-  useEffect(() => async () => {
-    await getPost(id);
-  }, [reload]);
 
   const deletePost = async (posts) => {
     await blogFetch.delete(`/posts/${posts}`);
@@ -34,24 +32,28 @@ function ReadMore() {
     return `${newDate.getDate()} de ${longMonth} ás ${hour}`;
   }
 
+  useEffect(() => async () => {
+    await getPost(id);
+  }, [reload]);
+
   return (
     <div className="read-more">
+      <h1>Detalhes do post</h1>
       {!post ? (
         <p>Post não encontrado</p>
-      ) : (post.map((posts) => (
+      ) : (
         <div className="post" key={post.id}>
           <h2>{post.title}</h2>
           <p className="span-container">
-            <span>{`${posts.owner} `}</span>
-            <span>{getDate(posts.created_at)}</span>
+            <span>{`${post.owner} `}</span>
+            <span>{getDate(post.created_at)}</span>
           </p>
-          <p className="quebra-linha">{posts.body}</p>
+          <p className="quebra-linha">{post.body}</p>
           <div className="container-btn">
             <GoTo post={post} deletePost={deletePost} />
             <DeleteBtn post={post} deletePost={deletePost} />
           </div>
         </div>
-      ))
       )}
     </div>
   );
